b5ef45f08d63ead560271149f0bddff5b5edc82a
Dong Cong Dinh <dinhdc1111@gmail.com>
2025-03-26 09:26:31 +0700

    test: :test_tube: your first react native app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Demo React Native Project</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={{
+      flex: 1,
+      backgroundColor: '#fff',
+    }}>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text>Some text</Text>
+          <Text>Some more text</Text>
+          <Image source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png' }} style={{
+            width: 200,
+            height: 200
+          }} />
+          <StatusBar style="auto" />
+        </View>
+        <TextInput style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+          defaultValue="You can type in me" />
+      </ScrollView>
+    </ScrollView>
   );
 }
 
@@ -16,5 +34,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 80
   },
 });
