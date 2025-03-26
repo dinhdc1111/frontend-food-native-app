6c030e7e37f6fb3a4be78b4b0606711ce064fdee
Dong Cong Dinh <dinhdc1111@gmail.com>
2025-03-26 17:48:48 +0700

    test: :test_tube: Core component InputText

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [products, setProducts] = useState([{
+    id: 1,
+    name: 'Product 1',
+    price: 10.99,
+    image: 'https://reactnative.dev/docs/assets/p_cat1.png',
+  }])
+  const [text, onChangeText] = useState("Useless Text")
   return (
     <ScrollView style={{
       flex: 1,
@@ -16,13 +23,37 @@ export default function App() {
             height: 200
           }} />
         </View>
-        <TextInput style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1,
-          margin: 20,
-        }}
-          defaultValue="You can type in me" />
+        <TextInput style={styles.input}
+          // keyboardType='numeric'
+          // maxLength={6}
+          // multiline
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder='Nhập thông tin tìm kiếm'
+          onChangeText={onChangeText}
+          value={text}
+        />
+        <Text>Kết quả tìm kiếm: {text}</Text>
+        <View>
+          {
+            products.map((product) => (
+              <View key={product.id} style={{
+                margin: 10,
+                padding: 10,
+                borderWidth: 1,
+                borderColor: '#ccc',
+                borderRadius: 5,
+              }}>
+                <Text>{product.name}</Text>
+                <Text>{product.price} USD</Text>
+                <Image source={{ uri: product.image }} style={{
+                  width: 100,
+                  height: 100
+                }} />
+              </View>
+            ))
+          }
+        </View>
       </ScrollView>
     </ScrollView>
   );
@@ -42,5 +73,12 @@ const styles = StyleSheet.create({
   },
   textSecondary: {
     color: '#58c4dc'
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 20,
+    padding: 10
   }
 });
