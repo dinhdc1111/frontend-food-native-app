ac6ddf44c11887c4ac319c9f871a21e815492063
Dong Cong Dinh <dinhdc1111@gmail.com>
2025-03-26 21:09:13 +0700

    test: :test_tube: Core component Button and ScrollView

--- App.tsx
+++ App.tsx
@@ -1,60 +1,61 @@
 import { useState } from 'react';
-import { Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const [products, setProducts] = useState([{
-    id: 1,
-    name: 'Product 1',
-    price: 10.99,
-    image: 'https://reactnative.dev/docs/assets/p_cat1.png',
-  }])
+  const [players, setPlayers] = useState([
+    { id: 1, name: 'Thibaut Courtois' },
+    { id: 2, name: 'Ricardo Carvalho' },
+    { id: 3, name: 'Ángel Di María' },
+    { id: 4, name: 'Luka Modrić' },
+    { id: 5, name: 'Isco' },
+    { id: 6, name: 'Mateo Kovačić' },
+    { id: 7, name: 'Cristiano Ronaldo' },
+    { id: 8, name: 'Toni Kroos' },
+    { id: 9, name: 'Lucas Vázquez' },
+    { id: 10, name: 'Eden Hazard' },
+    { id: 11, name: 'Antonio Rüdiger' },
+  ])
   const [text, onChangeText] = useState("Useless Text")
   return (
     <ScrollView style={{
       flex: 1,
       backgroundColor: '#fff',
     }}>
-      <ScrollView>
-        <View style={styles.container}>
-          <Text style={styles.text}>React <Text style={styles.textSecondary}>Native</Text></Text>
-          <Text>Learn once, write anywhere</Text>
-          <Image source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png' }} style={{
-            width: 200,
-            height: 200
-          }} />
-        </View>
-        <TextInput style={styles.input}
-          // keyboardType='numeric'
-          // maxLength={6}
-          // multiline
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder='Nhập thông tin tìm kiếm'
-          onChangeText={onChangeText}
-          value={text}
-        />
-        <Text>Kết quả tìm kiếm: {text}</Text>
-        <View>
-          {
-            products.map((product) => (
-              <View key={product.id} style={{
-                margin: 10,
-                padding: 10,
-                borderWidth: 1,
-                borderColor: '#ccc',
-                borderRadius: 5,
-              }}>
-                <Text>{product.name}</Text>
-                <Text>{product.price} USD</Text>
-                <Image source={{ uri: product.image }} style={{
-                  width: 100,
-                  height: 100
-                }} />
-              </View>
-            ))
-          }
-        </View>
-      </ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.text}>React <Text style={styles.textSecondary}>Native</Text></Text>
+        <Text>Learn once, write anywhere</Text>
+        <Image source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png' }} style={{
+          width: 200,
+          height: 200
+        }} />
+      </View>
+      <TextInput style={styles.input}
+        // keyboardType='numeric'
+        // maxLength={6}
+        // multiline
+        autoCorrect={false}
+        autoCapitalize="none"
+        placeholder='Nhập thông tin tìm kiếm'
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Text style={{ marginVertical: 10 }}>Kết quả tìm kiếm: {text}</Text>
+      <Button title='Thêm mới' onPress={() => alert('Tap me...')} />
+      <View style={{ marginVertical: 10 }}>
+        {
+          players.map((player) => (
+            <View key={player.id} style={{
+              margin: 10,
+              padding: 10,
+              borderWidth: 1,
+              borderColor: '#ccc',
+              borderRadius: 5,
+            }}>
+              <Text>{player.name}</Text>
+            </View>
+          ))
+        }
+      </View>
     </ScrollView>
   );
 }
