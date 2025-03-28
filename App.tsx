9a9af21902ce631fe673fa05ea3e7ace4512d501
Dong Cong Dinh <dinhdc1111@gmail.com>
2025-03-28 11:37:58 +0700

    feat: :sparkles: Passing parameters to routes

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { NavigationContainer, useNavigation, useRoute } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Button, Text, View } from "react-native";
 
@@ -13,10 +13,10 @@ function HomeScreen() {
         <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
       </View>
       <View style={{ marginVertical: 10 }}>
-        <Button title="Go to product ID 1" onPress={() => navigation.navigate('Details')} />
+        <Button title="Go to product ID 1" onPress={() => navigation.navigate('Details', { userId: 1, name: "Product 1" })} />
       </View>
       <View style={{ marginVertical: 10 }}>
-        <Button title="Go to product ID 2" onPress={() => navigation.navigate('Details')} />
+        <Button title="Go to product ID 2" onPress={() => navigation.navigate('Details', { userId: 2, name: "Product 2" })} />
       </View>
     </View>
   );
@@ -24,13 +24,16 @@ function HomeScreen() {
 
 function DetailsScreen() {
   const navigation: any = useNavigation();
+  const route: any = useRoute();
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
       <View style={{ marginVertical: 10 }}>
         <Button title="Go back" onPress={() => navigation.goBack()} />
       </View>
-      <Text>ID Product = ...</Text>
+      <Text>ID: {route.params.userId}</Text>
+      <Text>Tên sản phẩm: {route.params.name}</Text>
     </View>
   );
 }
