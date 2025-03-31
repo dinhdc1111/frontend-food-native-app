04d4b9fabc273a189d1a9f05a473a2f91107f4f5
Dong Cong Dinh <dinhdc1111@gmail.com>
2025-04-01 00:10:35 +0700

    feat: :sparkles: Build UI Welcome page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,88 +1,101 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native'
 import AppButton from 'components/button/AppButton'
 import { APP_COLOR } from 'utils/constant'
 import Ionicons from '@expo/vector-icons/Ionicons';
+import backgroundImage from '@/assets/auth/welcome-background.png';
+import facebookIcon from '@/assets/auth/facebook.png';
+import googleIcon from '@/assets/auth/google.png';
+import { LinearGradient } from 'expo-linear-gradient';
+import TextBetweenLine from '@/components/text/TextBetweenLine';
 type Props = {}
 const WelcomePage = (props: Props) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.welcomeContent}>
-        <Text style={styles.header}>Welcome to</Text>
-        <Text style={styles.brand}>ShopeeFood</Text>
-        <Text style={styles.description}>Your favourite foods delivered fast at your door.</Text>
-      </View>
-      <View style={styles.welcomeGroupButton}>
-        <View style={{
-          borderBottomWidth: 1,
-          borderBottomColor: "red",
-          marginHorizontal: 50
-        }}>
-          <Text style={{
-            padding: 10,
-            textAlign: 'center',
-            backgroundColor: "white",
-            alignSelf: "center",
-            position: "relative",
-            top: 20
-          }}>Sign in with</Text>
-        </View>
-        <View style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          marginHorizontal: 40,
-        }}>
-          <AppButton
-            title='facebook'
-            onPress={() => alert(1)}
-            textStyle={{ textTransform: "uppercase" }}
-            btnStyle={{ justifyContent: "center", backgroundColor: "#fff", }}
-            icons={(<Ionicons name="add-circle" size={30} color="#2c2c2c" />)}
-          />
-          <AppButton
-            title='google'
-            onPress={() => alert(1)}
-            textStyle={{ textTransform: "uppercase" }}
-            btnStyle={{
-              backgroundColor: "#fff",
-              justifyContent: "center",
-              paddingHorizontal: 22
-            }}
-            icons={(<Ionicons name="add-circle" size={30} color="#2c2c2c" />)}
-          />
-        </View>
-        <View>
-          <AppButton
-            title='Start with your email'
-            onPress={() => alert(1)}
-            textStyle={{ color: "#fff", paddingVertical: 5 }}
-            pressStyle={{ alignSelf: "stretch" }}
-            btnStyle={{
-              backgroundColor: "#2c2c2c",
-              justifyContent: "center",
-              paddingHorizontal: 22,
+    <ImageBackground
+      style={{ flex: 1 }}
+      source={backgroundImage}
+    >
+      <LinearGradient
+        style={{ flex: 1 }}
+        colors={['transparent', '#191B2F']}
+        locations={[0.1, 0.8]}
+      >
+        <View style={styles.container}>
+          <View style={styles.welcomeContent}>
+            <Text style={styles.header}>Welcome to</Text>
+            <Text style={styles.brand}>ShopeeFood</Text>
+            <Text style={styles.description}>Your favourite foods delivered fast at your door.</Text>
+          </View>
+          <View style={styles.welcomeGroupButton}>
+            <TextBetweenLine title='Sign in with' />
+            <View style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
               marginHorizontal: 40,
-              maxWidth: 315
-            }}
-          />
+            }}>
+              <AppButton
+                title='facebook'
+                onPress={() => alert(1)}
+                textStyle={{ textTransform: "uppercase" }}
+                btnStyle={{ justifyContent: "center", backgroundColor: "#fff", }}
+                icons={<Image source={facebookIcon} />}
+              />
+              <AppButton
+                title='google'
+                onPress={() => alert(1)}
+                textStyle={{ textTransform: "uppercase" }}
+                btnStyle={{
+                  backgroundColor: "#fff",
+                  justifyContent: "center",
+                  paddingHorizontal: 22
+                }}
+                icons={<Image source={googleIcon} />}
+              />
+            </View>
+            <View>
+              <AppButton
+                title='Start with your email'
+                onPress={() => alert(1)}
+                textStyle={{ color: "#fff", paddingVertical: 5 }}
+                pressStyle={{ alignSelf: "stretch" }}
+                btnStyle={{
+                  backgroundColor: "#2c2c2c",
+                  justifyContent: "center",
+                  paddingHorizontal: 22,
+                  marginHorizontal: 40,
+                  borderWidth: 1,
+                  borderColor: "#505050",
+                  maxWidth: 315
+                }}
+              />
+            </View>
+            <View style={{
+              flexDirection: "row",
+              gap: 10,
+              justifyContent: "center"
+            }}>
+              <Text style={{
+                color: "white",
+              }}>
+                Chưa có tài khoản?
+              </Text>
+              <Text style={{ color: "white", textDecorationLine: 'underline' }}>
+                Đăng ký.
+              </Text>
+            </View>
+          </View>
         </View>
-        <View>
-          <Text style={{ textAlign: "center" }}>Already have an account? Sign In</Text>
-        </View>
-      </View>
-    </View>
+      </LinearGradient>
+    </ImageBackground>
   )
 }
 export default WelcomePage
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    borderColor: "red",
-    borderWidth: 5
+    paddingHorizontal: 10
   },
   welcomeContent: {
     flex: .6,
-    borderColor: "blue",
-    borderWidth: 5,
     alignItems: "flex-start",
     justifyContent: "center",
     padding: 28,
@@ -104,8 +117,6 @@ const styles = StyleSheet.create({
   },
   welcomeGroupButton: {
     flex: .4,
-    borderColor: "green",
-    borderWidth: 5,
-    gap: 20
+    gap: 30
   },
 })
